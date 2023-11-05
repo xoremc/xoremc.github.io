67bf2373e466209dbdd82109521478d1a8384d5f
xoremc <149946930+xoremc@users.noreply.github.com>
2023-11-05 18:54:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// Get references to the button and popup elements
+const showPopupButton = document.getElementById("showPopup");
+const showPopupButton2 = document.getElementById("showPopup2");
+const showPopupButton3 = document.getElementById("showPopup3");
+const showPopupButton4 = document.getElementById("showPopup4");
+const showPopupButton5 = document.getElementById("showPopup5");
+const showPopupButton6 = document.getElementById("showPopup6");
+const closePopupButton = document.getElementById("closePopup");
+
+// Show the popup when the button is clicked
+showPopupButton.addEventListener("click", function() {
+    popup.style.display = "block";
+});
+
+// Close the popup when the close button is clicked
+closePopupButton.addEventListener("click", function() {
+    popup.style.display = "none";
+});
+
+
+// Show the popup when the button is clicked
+showPopupButton2.addEventListener("click", function() {
+    popup.style.display = "block";
+});
+
+showPopupButton3.addEventListener("click", function() {
+    popup.style.display = "block";
+});
+
+showPopupButton4.addEventListener("click", function() {
+    popup.style.display = "block";
+});
+
+showPopupButton5.addEventListener("click", function() {
+    popup.style.display = "block";
+});
+
+showPopupButton6.addEventListener("click", function() {
+    popup.style.display = "block";
+});
+
